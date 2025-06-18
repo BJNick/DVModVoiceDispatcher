f24eca56f11d7fbfa2f8dce4910cc386d7bb7fea
Mykyta Shvets <nikitashets28@gmail.com>
2025-06-17 18:13:57 -0600

    Add voice preview in the project

---
+++ UnityAssetBundle/Assets/VoicePreview.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class VoicePreview : MonoBehaviour
+{
+    private const string SHORT_PAUSE = "ShortSilence";
+    private static AudioSource audioSource;
+    
+    private static AssetBundle voicedLines;
+    
+    public bool play = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        voicedLines = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/voiced_lines");
+    }
+
+    void Update() {
+        if (play) { 
+            play = false;
+            
+            var lineBuilder = new List<string>();
+            AddShuntingLoadJobLines(lineBuilder);
+
+            var line = string.Join(" ", lineBuilder);
+            Debug.Log(line);
+            StartCoroutine(PlayVoiceLinesCoroutine(lineBuilder.ToArray()));
+        }
+}
+    
+    private static void AddShuntingLoadJobLines(List<string> lineBuilder) {
+        lineBuilder.AddRange(new[] {
+            "YouHave", "JobTypeShuntingLoad", SHORT_PAUSE,
+            "Couple", "4Cars", "AtTrack", "D", "1", SHORT_PAUSE,
+            "2Cars", "AtTrack", "A", "5", SHORT_PAUSE,
+            "ThenMove", "6Cars", "ToTrack", "C", "3", "ForLoading", SHORT_PAUSE,
+            "ThenUncouple", "AtTrackTypeO", "B", "7", "ForDeparture"
+        });
+    }
+    
+    static IEnumerator PlayVoiceLinesCoroutine(string[] lines) {
+        AudioClip[] clips = lines.Select(GetVoicedClip).Where(clip => clip != null).ToArray();
+        if (clips.Length == 0) {
+            Debug.LogError("No valid voice lines found.");
+            yield break;
+        }
+        foreach (var clip in clips) {
+            audioSource.PlayOneShot(clip);
+            // Sleep until the next clip is ready to play
+            var waitTime = clip.length - 0.05f;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+        
+    static AudioClip GetVoicedClip(string name)
+    {
+        if (voicedLines == null) {
+            Debug.LogError("Voiced lines asset bundle is not loaded.");
+            return null;
+        }
+        var clip = voicedLines.LoadAsset<AudioClip>(name);
+        if (clip == null) {
+            Debug.LogError("Failed to load voice line: " + name);
+        }
+        return clip;
+    }
+}

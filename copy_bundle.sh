2603c62c9ab8b015ce95ed02d67a378370d411ad
Mykyta Shvets <nikitashets28@gmail.com>
2025-06-28 08:58:11 -0600

    Add info.json

--- copy_bundle.sh
+++ copy_bundle.sh
@@ -1,3 +1,4 @@
 
-cp "D:\Projects\Mods\DVVoiceAssistant\UnityAssetBundle\AssetBundles\StandaloneWindows\voiced_lines" "D:\SteamLibrary\steamapps\common\Derail Valley\Mods\TestMod\voiced_lines"
+cp ".\UnityAssetBundle\AssetBundles\StandaloneWindows\voiced_lines" "D:\SteamLibrary\steamapps\common\Derail Valley\Mods\VoiceDispatcherMod\voiced_lines"
+cp ".\info.json" "D:\SteamLibrary\steamapps\common\Derail Valley\Mods\VoiceDispatcherMod\info.json"
 
